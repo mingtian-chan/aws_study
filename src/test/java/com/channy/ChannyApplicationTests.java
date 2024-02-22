ee21949aa6e03bd3dcd40325864bc5f0475c6cc8
mingtian-chan <sniwoo98@naver.com>
2024-02-22 11:27:03 +0900

    init

---
+++ src/test/java/com/channy/ChannyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.channy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
